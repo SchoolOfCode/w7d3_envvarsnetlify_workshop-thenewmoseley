a583e556d891064c33e5b494991eec49af6c9483
Rob <robhodgskin@gmail.com>
2021-02-17 16:37:06 +0000

    with pic

--- myapp/src/Components/App/index.js
+++ myapp/src/Components/App/index.js
@@ -5,16 +5,19 @@ import './App.css';
 function App() {
 
   const [pokemon, setPokemon] = useState("");
+  const [pokeImg, setPokeImg] = useState();
 
   useEffect(()=> {
     fetchPoke();
   }, []);
 
   async function fetchPoke() {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const res = await fetch(process.env.REACT_APP_API_URL);
     const data = await res.json();
     console.log(data); //why we cannot retrieve object data alone?
     setPokemon(data.name);
+    console.log(data.sprites);
+    setPokeImg(data.sprites.front_default);
   }
 
   function handleClick () {
@@ -24,9 +27,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="button" onClick={handleClick}/>
-        <p>{pokemon}</p>
+        <img src={pokeImg} className="App-logo" alt="logo" />
+        <button className="button" onClick={handleClick}>click me</button>
+        <h1>name: {pokemon}</h1>
       </header>
     </div>
   );
