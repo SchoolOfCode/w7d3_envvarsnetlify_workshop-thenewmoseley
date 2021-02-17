0c86ab4df7782a1628cc931c56d024a792b2d651
Rob <robhodgskin@gmail.com>
2021-02-17 16:00:11 +0000

    getting the API to work

--- myapp/src/Components/App/index.js
+++ myapp/src/Components/App/index.js
@@ -1,35 +1,32 @@
+import {useState, useEffect} from 'react';
 import logo from './../../logo.svg';
 import './App.css';
 
 function App() {
 
-  const [joke, setJoke] = useState("");
+  const [pokemon, setPokemon] = useState("");
 
   useEffect(()=> {
-    fetchJoke();
+    fetchPoke();
   }, []);
 
-  async function fetchJoke() {
-    const res = await fetch(process.env.REACT_APP_DAD_JOKES_URL)
+  async function fetchPoke() {
+    const res = await fetch(process.env.REACT_APP_DAD_JOKES_URL);
     const data = await res.json();
-    setJoke(data.joke)
+    console.log(data);
+    setPokemon(data)
+  }
+
+  function handleClick () {
+    fetchPoke();
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="button" onClick={handleClick}/>
+        <p>{pokemon.id}</p>
       </header>
     </div>
   );
